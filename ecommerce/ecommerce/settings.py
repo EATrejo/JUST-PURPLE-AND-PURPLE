6659badf1c6bef5e61845132594c76f391619f64
Alonso Trejo <alonsotrejo1970@hotmail.com>
2025-05-19 13:13:35 -0600

    Cambio en settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -159,6 +159,20 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'static/media'
 
+######### MODIFICACIN STATICS PARA PYTHON ANYWHERE
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files (user uploaded)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')  # or just 'media'
+]
+
 
 
 # Default primary key field type
